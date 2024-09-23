ac5774ecffb63127f17b0ccf927bd187d274bfb7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-23 20:41:03 +0000

    added completed page draft

--- lib/index.dart
+++ lib/index.dart
@@ -2,3 +2,4 @@
 export '/login/login_widget.dart' show LoginWidget;
 export '/tasks/tasks_widget.dart' show TasksWidget;
 export '/onboarding/onboarding_widget.dart' show OnboardingWidget;
+export '/completed/completed_widget.dart' show CompletedWidget;
